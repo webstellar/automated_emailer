bdc67a0f70e616d36a45cd1d1d7011b8ad64f642
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2023-10-12 14:07:36 +0100

    Final Update

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -2,10 +2,11 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
 const sender = process.env.NODEMAILER_USER;
+const pass = process.env.NODEMAILER_PASS;
 
 export const transport = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
-  port: Number(process.env.NODEMAILER_PORT),
+  port: 465,
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
@@ -14,5 +15,4 @@ export const transport = nodemailer.createTransport({
 
 export const mailOptions = {
   from: email,
-  to: "pete.onyegbule@gmail.com",
 };
